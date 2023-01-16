a1f0428ee6dd5801f13dc19a061abdf7dd22071b
GOWTHAM M H <gowthammh02@gmail.com>
2023-01-16 11:10:37 +0530

    Solution of polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(10,5);
+        p.product(10,5,3);
+        p.product(10.08,5.65);
+    }
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
